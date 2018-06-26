5a56ec440e456847394ed62e6c6a3f57694468ff
Michael Hogg <mh@enterprisealumni.com>
2018-06-25 21:40:25 -0700

    adding brew provisioning script.

---
+++ prov.sh
@@ -0,0 +1,71 @@
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install evernote
+brew cask install steam discord
+brew cask install iterm2
+brew cask install caffeine
+brew cask install cyberduck
+brew install coreutils
+brew install findutils
+brew install git
+brew install ack
+brew tap homebrew/dupes
+brew install grep
+brew install wget 
+brew install libvpx
+brew install ffmpeg --with-libvpx
+brew install imagemagick
+brew install rbenv
+brew install rbenv-gem-rehash
+brew install ruby-build
+brew install fortune
+brew install pyenv
+brew install awscli 
+brew install links
+brew install geoiplookup
+brew install nmap
+brew install watch
+brew install hh
+brew install htop
+brew install irssi
+brew install jq
+brew install tig
+brew install tmux
+brew install unrar
+brew install thefuck
+brew install trash
+brew install archey
+brew install hub
+brew install zplug
+brew install cowsay
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+brew cask install betterzipql
+brew cask install webpquicklook
+brew cask install suspicious-package
+brew cask install coconutbattery
+brew cask install jumpcut
+brew cask install commandq
+brew cask install virtualbox vagrant
+brew cask install dashlane docker slack
+brew cask install docker
+brew cask install slack
+brew cask install spotify
+brew cask install sourcetree
+brew cask install timing
+brew cask install 1password 1password-cli
+brew cask install zoom
+brew install yarn
+brew tap homebrew/cask-fonts 
+sudo easy_install pip
+pip install boto3 ptpython --user
+brew install homebrew/cask-fonts/font-awesome-terminal-fonts homebrew/cask-fonts/font-3270-nerd-font homebrew/cask-fonts/font-gohu-nerd-font homebrew/cask-fonts/font-monoid-nerd-font homebrew/cask-fonts/font-3270-nerd-font-mono homebrew/cask-fonts/font-gohu-nerd-font-mono homebrew/cask-fonts/font-monoid-nerd-font-mono homebrew/cask-fonts/font-anonymouspro-nerd-font homebrew/cask-fonts/font-hack-nerd-font homebrew/cask-fonts/font-mononoki-nerd-font homebrew/cask-fonts/font-anonymouspro-nerd-font-mono homebrew/cask-fonts/font-hack-nerd-font-mono homebrew/cask-fonts/font-mononoki-nerd-font-mono homebrew/cask-fonts/font-arimo-nerd-font homebrew/cask-fonts/font-hasklig-nerd-font homebrew/cask-fonts/font-mplus-nerd-font homebrew/cask-fonts/font-arimo-nerd-font-mono homebrew/cask-fonts/font-hasklig-nerd-font-mono homebrew/cask-fonts/font-mplus-nerd-font-mono homebrew/cask-fonts/font-aurulentsansmono-nerd-font homebrew/cask-fonts/font-heavydata-nerd-font homebrew/cask-fonts/font-profont-nerd-font homebrew/cask-fonts/font-aurulentsansmono-nerd-font-mono homebrew/cask-fonts/font-heavydata-nerd-font-mono homebrew/cask-fonts/font-profont-nerd-font-mono homebrew/cask-fonts/font-bitstreamverasansmono-nerd-font homebrew/cask-fonts/font-hermit-nerd-font homebrew/cask-fonts/font-proggyclean-nerd-font homebrew/cask-fonts/font-bitstreamverasansmono-nerd-font-mono homebrew/cask-fonts/font-hermit-nerd-font-mono homebrew/cask-fonts/font-proggyclean-nerd-font-mono homebrew/cask-fonts/font-codenewroman-nerd-font homebrew/cask-fonts/font-inconsolata-nerd-font homebrew/cask-fonts/font-robotomono-nerd-font homebrew/cask-fonts/font-codenewroman-nerd-font-mono homebrew/cask-fonts/font-inconsolata-nerd-font-mono homebrew/cask-fonts/font-robotomono-nerd-font-mono homebrew/cask-fonts/font-cousine-nerd-font homebrew/cask-fonts/font-inconsolatago-nerd-font homebrew/cask-fonts/font-sharetechmono-nerd-font homebrew/cask-fonts/font-cousine-nerd-font-mono homebrew/cask-fonts/font-inconsolatago-nerd-font-mono homebrew/cask-fonts/font-sharetechmono-nerd-font-mono homebrew/cask-fonts/font-dejavusansmono-nerd-font homebrew/cask-fonts/font-inconsolatalgc-nerd-font homebrew/cask-fonts/font-sourcecodepro-nerd-font homebrew/cask-fonts/font-dejavusansmono-nerd-font-mono homebrew/cask-fonts/font-inconsolatalgc-nerd-font-mono homebrew/cask-fonts/font-sourcecodepro-nerd-font-mono homebrew/cask-fonts/font-droidsansmono-nerd-font homebrew/cask-fonts/font-iosevka-nerd-font homebrew/cask-fonts/font-spacemono-nerd-font homebrew/cask-fonts/font-droidsansmono-nerd-font-mono homebrew/cask-fonts/font-iosevka-nerd-font-mono homebrew/cask-fonts/font-spacemono-nerd-font-mono homebrew/cask-fonts/font-fantasquesansmono-nerd-font homebrew/cask-fonts/font-lekton-nerd-font homebrew/cask-fonts/font-terminus-nerd-font homebrew/cask-fonts/font-fantasquesansmono-nerd-font-mono homebrew/cask-fonts/font-lekton-nerd-font-mono homebrew/cask-fonts/font-terminus-nerd-font-mono homebrew/cask-fonts/font-firacode-nerd-font homebrew/cask-fonts/font-liberationmono-nerd-font homebrew/cask-fonts/font-tinos-nerd-font homebrew/cask-fonts/font-firacode-nerd-font-mono homebrew/cask-fonts/font-liberationmono-nerd-font-mono homebrew/cask-fonts/font-tinos-nerd-font-mono homebrew/cask-fonts/font-firamono-nerd-font homebrew/cask-fonts/font-meslo-nerd-font homebrew/cask-fonts/font-ubuntu-nerd-font homebrew/cask-fonts/font-firamono-nerd-font-mono homebrew/cask-fonts/font-meslo-nerd-font-mono homebrew/cask-fonts/font-ubuntu-nerd-font-mono homebrew/cask-fonts/font-go-mono-nerd-font homebrew/cask-fonts/font-monofur-nerd-font homebrew/cask-fonts/font-ubuntumono-nerd-font homebrew/cask-fonts/font-go-mono-nerd-font-mono homebrew/cask-fonts/font-monofur-nerd-font-mono homebrew/cask-fonts/font-ubuntumono-nerd-font-mono
+sudo gem update --system
+gem install pry pry-remote pry_debug awesome_print activesupport business_time lolcat cowsay --user-install
+
